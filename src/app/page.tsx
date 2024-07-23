0cc8339498c0aa45313e661825843e166e8c1b01
andersonruizr2 <andersonruizr2@gmail.com>
2024-07-23 22:20:49 +0000

    update file home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import FormJob from "@/components/ui/form/FormJob";
-import JobForm from "@/components/ui/form/JobForm";
 
 export default function Home() {
   return (
     <>
       {/* <h2>Welcome Working Labs</h2> */}
-      {/* <JobForm /> */}
       <FormJob />
     </>
   );
